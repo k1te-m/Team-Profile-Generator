0ad02a7afa34240e5133693bdc0a3b168dcfa408
k1te <kmiller343@gmail.com>
2020-10-20 14:40:23 -0500

    set up initial classes

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,23 @@
-// TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name, id, email) {
+
+    }
+
+    getName() {
+
+    }
+
+    getId() {
+
+    }
+    
+    getEmail() {
+
+    }
+
+    getRole() {
+        
+    }
+}
+
+module.exports = Employee;
