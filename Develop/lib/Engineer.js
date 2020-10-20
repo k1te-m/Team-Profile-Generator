f366ced77bf89540864deba9cf58ba73eab607f4
k1te <kmiller343@gmail.com>
2020-10-20 15:07:23 -0500

    created engineer class extended off employee

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,5 +1,16 @@
-class Engineer {
-    
+class Engineer extends Employee {
+    constructor(name, id, email, github) {
+        super(name, id, email);
+        this.github = github;
+    }
+
+    getRole() {
+        return "Engineer";
+    }
+
+    getGitHub() {
+        return this.github;
+    }
 }
 
 module.exports = Engineer;
