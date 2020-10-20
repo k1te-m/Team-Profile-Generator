0036416c1a68cf22b7a97e75bd02bee7cb941d59
k1te <kmiller343@gmail.com>
2020-10-20 14:43:51 -0500

    created methods for employee class

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -11,15 +11,15 @@ class Employee {
     }
 
     getId() {
-
+        return this.id;
     }
     
     getEmail() {
-
+        return this.email;
     }
 
     getRole() {
-
+        return "Employee";
     }
 }
 
